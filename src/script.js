9c67db1ac83ff5551d2c5db031ec1316beb2b676
Heang <heangt@users.noreply.github.com>
2022-10-29 12:52:51 +0200

    Updated  h1 by using WEATHER API provided

--- src/script.js
+++ src/script.js
@@ -22,14 +22,14 @@ function showTemperature(response) {
   let Temperature = Math.round(response.data.main.temp);
   let degrees = document.querySelector("#temperature");
   degrees.innerHTML = Temperature;
+  let showCity = document.querySelector("h1");
+  showCity.innerHTML=response.data.name;
+   
 }
 function displayCity(event) {
   event.preventDefault();
   let searchCity = document.querySelector("#input-city");
-  let showCity = document.querySelector("h1");
   let nameCity = `${searchCity.value}`;
-  showCity.innerHTML = `${nameCity}`;
-
   let apiKey = "57821c3b75b60c68ecd1a8d0dd1aa8d3";
   let apiWeather = `https://api.openweathermap.org/data/2.5/weather?q=${nameCity}&units=metric&appid=${apiKey}`;
   axios.get(apiWeather).then(showTemperature);
