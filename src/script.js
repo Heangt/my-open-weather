fb84ca3651a275e142e100e87054f99d05a19752
Heang <heangt@users.noreply.github.com>
2022-10-29 19:06:57 +0200

    cleansed the showTemp function using the previous function

--- src/script.js
+++ src/script.js
@@ -1,17 +1,10 @@
-function showTemp(response) {
-  
-  let showCity = document.querySelector("h1");
-  showCity.innerHTML = response.data.name;
-  let degrees = document.querySelector("#temperature");
-  degrees.innerHTML = Math.round(response.data.main.temp);
-  
-}
+
 function getPosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiKey = "57821c3b75b60c68ecd1a8d0dd1aa8d3";
   let apiGeoCoordinate = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiGeoCoordinate).then(showTemp);
+  axios.get(apiGeoCoordinate).then(showTemperature);
 }
 function showCurrentTemperature() {
   navigator.geolocation.getCurrentPosition(getPosition);
