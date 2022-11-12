649b570ed21ed30d86479fc1921f09e980ae6a19
Heang <heangt@users.noreply.github.com>
2022-11-12 19:11:00 +0100

    Display the forecast data using the API

--- src/script.js
+++ src/script.js
@@ -23,8 +23,13 @@ let currentMinutes = currentDate.getMinutes();
 if (currentMinutes < 10){
 currentMinutes =`0${currentMinutes}`;
 }
+
+let currentDay = days[currentDate.getDay()];
+
+return`${currentDay} ${currentHour}:${currentMinutes}`;
+}
 let days = [
-  "sunday",
+  "Sunday",
   "Monday",
   "Tuesday",
   "Wednesday",
@@ -32,19 +37,46 @@ let days = [
   "Friday",
   "Saturday"
 ];
-let currentDay = days[currentDate.getDay()];
 
-return`${currentDay} ${currentHour}:${currentMinutes}`;
+function displayForecast(response){
+  let forecastElement = document.querySelector("#forecast");
+  let dailyForecast = response.data.daily;
+  let forecastHTML = `<div class="row">`;
+  
+  dailyForecast.forEach(function (forecastDay,index){
+    if (index < 5) 
+    forecastHTML=
+         forecastHTML + 
+         `<div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h2 class="card-title">${days[new Date(forecastDay.dt * 1000).getDay()]}</h2>
+              <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="weather-icon" class ="icon-weather">
+              <p class="card-text"><span class="temp-max">${Math.round(forecastDay.temp.max)}º</span> ${Math.round(forecastDay.temp.min)}º</p>
+            </div>
+          </div>
+        </div>`})
+  forecastHTML= forecastHTML + `</div>`;
+  forecastElement.innerHTML=forecastHTML;  
+       
 }
 
 
 
-
-
-
-function showTemperature(response) {
+function getForecast(coord){
   
- celsius = response.data.main.temp;
+  let latitude =coord.lat;
+  let longitude =coord.lon;
+  let  apiKey = "57821c3b75b60c68ecd1a8d0dd1aa8d3";
+  let apiForecast =`http://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+  axios.get(apiForecast).then(displayForecast);
+  
+
+}
+
+
+
+function showTemperature(response) { celsius = response.data.main.temp;
 let temperature = Math.round(celsius);
 let degrees = document.querySelector("#temperature");
 degrees.innerHTML = temperature;
@@ -60,6 +92,10 @@ let weatherIcon = response.data.weather[0].icon;
 let icon = document.querySelector("#weather-icon");
 icon.setAttribute("src", `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`);
 icon.setAttribute("alt",response.data.weather[0].main );
+let geoCoordinates =response.data.coord;
+getForecast(geoCoordinates);
+
+
 
 }
    function showDefaultCity (city){
